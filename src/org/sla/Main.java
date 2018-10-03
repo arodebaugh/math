11737046aa87e6d5ec12442b5f0acb49f28c5472
andrewrodebaugh <aquahydration@gmail.com>
2018-10-03 08:35:22 -0400

    args and switch

--- src/org/sla/Main.java
+++ src/org/sla/Main.java
@@ -5,27 +5,33 @@ public class Main {
     public static void main(String[] args) {
         // One Step
 
-        char symbol = '/';
+        String symbol = args[0];
         double first = 5;
         double second = 3;
         double last = 10;
 
         System.out.println("x " + symbol + " " + first + " = " + last);
 
-        if (symbol == '+') {
-            System.out.println("x = " + last + " - " + first);
-            System.out.println("x = " + Double.toString(last - first));
-        } else if (symbol == '-') {
-            System.out.println("x = " + last + " + " + first);
-            System.out.println("x = " + Double.toString(last + first));
-        } else if (symbol == '/') {
-            System.out.println("x = " + last + " * " + first);
-            System.out.println("x = " + Double.toString(last * first));
-        } else if (symbol == '*') {
-            System.out.println("x = " + last + " / " + first);
-            System.out.println("x = " + Double.toString(last / first));
-        } else {
-            System.out.println("Variable symbol must equal + - / or * only");
+        switch (symbol) {
+            case "+":
+                System.out.println("x = " + last + " - " + first);
+                System.out.println("x = " + Double.toString(last - first));
+                break;
+            case "-":
+                System.out.println("x = " + last + " + " + first);
+                System.out.println("x = " + Double.toString(last + first));
+                break;
+            case "/":
+                System.out.println("x = " + last + " * " + first);
+                System.out.println("x = " + Double.toString(last * first));
+                break;
+            case "*":
+                System.out.println("x = " + last + " / " + first);
+                System.out.println("x = " + Double.toString(last / first));
+                break;
+            default:
+                System.out.println("Variable symbol must equal + - / or * only");
+                break;
         }
 
         System.out.println(" ");
@@ -34,28 +40,34 @@ public class Main {
 
         System.out.println(first + "x " + symbol + " " + second + " = " + last);
 
-        if (symbol == '+') {
-            System.out.println(first + "x = " + last + " - " + second);
-            System.out.println(first + "x = " + Double.toString(last - second));
-            System.out.println("x = " + Double.toString(last - second) + " / " + first);
-            System.out.println("x = " + Double.toString((last - second) / first));
-        } else if (symbol == '-') {
-            System.out.println(first + "x = " + last + " + " + second);
-            System.out.println(first + "x = " + Double.toString(last + second));
-            System.out.println("x = " + Double.toString(last + second) + " / " + first);
-            System.out.println("x = " + Double.toString((last + second) / first));
-        } else if (symbol == '/') {
-            System.out.println(first + "x = " + last + " * " + second);
-            System.out.println(first + "x = " + Double.toString(last * second));
-            System.out.println("x = " + Double.toString(last * second) + " / " + first);
-            System.out.println("x = " + Double.toString((last * second) / first));
-        } else if (symbol == '*') {
-            System.out.println(first + "x = " + last + " / " + second);
-            System.out.println(first + "x = " + Double.toString(last / second));
-            System.out.println("x = " + Double.toString(last / second) + " / " + first);
-            System.out.println("x = " + Double.toString((last / second) / first));
-        } else {
-            System.out.println("Variable symbol must equal + - / or * only");
+        switch (symbol) {
+            case "+":
+                System.out.println(first + "x = " + last + " - " + second);
+                System.out.println(first + "x = " + Double.toString(last - second));
+                System.out.println("x = " + Double.toString(last - second) + " / " + first);
+                System.out.println("x = " + Double.toString((last - second) / first));
+                break;
+            case "-":
+                System.out.println(first + "x = " + last + " + " + second);
+                System.out.println(first + "x = " + Double.toString(last + second));
+                System.out.println("x = " + Double.toString(last + second) + " / " + first);
+                System.out.println("x = " + Double.toString((last + second) / first));
+                break;
+            case "/":
+                System.out.println(first + "x = " + last + " * " + second);
+                System.out.println(first + "x = " + Double.toString(last * second));
+                System.out.println("x = " + Double.toString(last * second) + " / " + first);
+                System.out.println("x = " + Double.toString((last * second) / first));
+                break;
+            case "*":
+                System.out.println(first + "x = " + last + " / " + second);
+                System.out.println(first + "x = " + Double.toString(last / second));
+                System.out.println("x = " + Double.toString(last / second) + " / " + first);
+                System.out.println("x = " + Double.toString((last / second) / first));
+                break;
+            default:
+                System.out.println("Variable symbol must equal + - / or * only");
+                break;
         }
     }
 }
